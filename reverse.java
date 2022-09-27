de1b389187be257b2433644e3a2f80557c8b2d1d
Sahi1437 <113417218+Sahi1437@users.noreply.github.com>
2022-09-27 01:53:34 -0700

    TESTED RESULTS
    1. INPUT=SIGN UP
     OUTPUT=P U   N G I S
    2.INPUT=AT-LEAST
    OUTPUT= T S A E L - T A
    3.INPUT=1245
    OUTPUT=error
    4.INPUT=!@#$%
    OUTPUT= % $ # @ !
    5.INPUT=145*999
    OUTPUT=error

---
+++ reverse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class reverse
+
+Scanner obj=new Scanner(System.in); System.out.println("Enter the word: ");
+
+String s obj.nextLine(); for(int i-s.length()-1;i>-0; i--){
+
+char c=s.charAt(i); if(c>=48 && c<=57){
+
+public static void main(String args[X System.out.print(ns);
+
+System.out.println("error"); break;
+
+} String ns="";
+
+ns=ns+c;
+
+} }
