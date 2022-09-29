54a7a1367a1d4e432d296cace5fa3df119da869d
Sahi1437 <113417218+Sahi1437@users.noreply.github.com>
2022-09-28 21:33:43 -0700

    Create perfect_square.java

---
+++ perfect_square.java
@@ -0,0 +1,33 @@
+import java.int
+public.
+if(math.sqrt(i)==(int)math.
+system.out.print
+for(inti=1;i<r;i
+import java.util.*; 
+class Main
+{
+static void perfectSquares(int l, int r)
+{
+for (int i = l; i <= r; i++)
+{
+if (Math.sqrt(i) == (int)Math.sqrt(i))
+System.out.print(i + " ");
+}
+}
+public static void main (String[] args)
+{
+try
+{
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Enter lower number: ");
+ int l=sc.nextInt();
+ System.out.println("Enter upper number: ");
+ int r=sc.nextInt();
+perfectSquares(l, r);
+}
+catch(Exception e)
+{
+    System.out.println("invalid input");
+}
+}
+}
